c20adfb397eecfc3cb4a7bcbd7d9c6e3f4a68f6c
Anshul <avenger720720@gmail.com>
2024-12-11 19:18:46 +0530

    working on js

---
+++ script.js
@@ -0,0 +1,8 @@
+const checkBoxList = document.querySelectorAll('.custom-checkbox ')
+
+checkBoxList.forEach((checkbox) => {
+    checkbox.addEventListener('click', (e) => {
+        checkbox.parentElement.classList.toggle('completed')
+        
+    })
+})

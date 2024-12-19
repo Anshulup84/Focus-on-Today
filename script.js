9752cd6324de519159d7afd92ad676910b4ac9f2
Anshul <avenger720720@gmail.com>
2024-12-19 17:13:11 +0530

    completed quotes

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
 const checkBoxList = document.querySelectorAll('.custom-checkbox')
 const inputFields = document.querySelectorAll('.goal-input')
 const errorLabel = document.querySelector('.error-label')
+const progressLabel = document.querySelector('.progress-label')
 const progressBar = document.querySelector('.progress-bar')
 const progressValue = document.querySelector('.progress-value')
 
+const allQuotes = [
+    'Raise the bar by completing your goals!',
+    'Well begun is half done!',
+    'Just a step away, keep going!',
+    'Whoa! You just completed all the goals, time for chill :D',
+]
+
 const allGoals = JSON.parse(localStorage.getItem('allGoals')) || {}
+let completedGoalsCount = Object.values(allGoals).filter((goal) => goal.completed).length
+
+progressValue.style.width = `${completedGoalsCount / 3 * 100}%`
+progressValue.firstElementChild.innerText = `${completedGoalsCount}/3 completed`
+progressLabel.innerText = allQuotes[completedGoalsCount]
 
 checkBoxList.forEach((checkbox) => {
     checkbox.addEventListener('click', (e) => {
@@ -14,8 +27,15 @@ checkBoxList.forEach((checkbox) => {
 
         if(allGoalsAdded) {
             checkbox.parentElement.classList.toggle('completed')
-            progressValue.style.width = "33.33%"
-            const inputId = checkbox.nextElementSibling
+            const inputId = checkbox.nextElementSibling.id
+            allGoals[inputId].completed = !allGoals[inputId].completed
+            completedGoalsCount = Object.values(allGoals).filter((goal) => goal.completed).length
+            
+            progressValue.style.width = `${completedGoalsCount / 3 * 100}%`
+            progressValue.firstElementChild.innerText = `${completedGoalsCount}/3 completed`
+            progressLabel.innerText = allQuotes[completedGoalsCount]
+
+            localStorage.setItem('allGoals', JSON.stringify(allGoals))
         } else {
             progressBar.classList.add('show-error')
         }
@@ -36,6 +56,11 @@ inputFields.forEach((input) => {
     })
 
     input.addEventListener('input', (e) => {
+      if(allGoals[input.id].completed) {
+      input.value = allGoals[input.id].name
+      return
+        }
+
     allGoals[input.id] = {
         name: input.value,
         completed: false,
